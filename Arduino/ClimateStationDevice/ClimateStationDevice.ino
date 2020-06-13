3177e914198900bbccaf85d9c0b1ebe89bc96032
jamespercy <james007percy@gmail.com>
2020-06-13 23:16:23 +1000

    reads env paramters from EEPROM

--- Arduino/ClimateStationDevice/ClimateStationDevice.ino
+++ Arduino/ClimateStationDevice/ClimateStationDevice.ino
@@ -7,6 +7,7 @@
 #include <Adafruit_Sensor.h>
 #include <Arduino.h>
 #include <DHT.h>
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
@@ -23,18 +24,6 @@ const char* host = "us-central1-climate-station.cloudfunctions.net";
 const char* deviceVersion = "ClimateStation-2016-06-06";
 const int httpsPort = 443;
 
-// Use web browser to view and copy
-// SHA1 fingerprint of the certificate
-const char fingerprint[] PROGMEM = "6D A7 25 5D 9F AD 12 7E DF 5A 9B 00 A0 8A 3C 9F 8D 7F BD DF";
-
-#define S1_DHTPIN 14  // Digital pin connected to the DHT sensor
-#define S2_DHTPIN 12  // Digital pin connected to the DHT sensor
-
-// Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
 //Fan controll definitions
 
 #define FanIA 5 //fan conrol input A blue
@@ -47,24 +36,53 @@ const char fingerprint[] PROGMEM = "6D A7 25 5D 9F AD 12 7E DF 5A 9B 00 A0 8A 3C
 #define PWM_FAST 200 // arbitrary fast speed PWM duty cycle
 #define PWM_FULL 255 // 255 is the new over 9000
 
+const unsigned int FLASH_STOREAGE_BYTES = 512;
+
+//Environment control variables
+const unsigned int DEFAULT_MAX_HUMIDITY = 99; //time to exchange some air
+const unsigned int DEFAULT_MIN_HUMIDITY = 90; //getting a bit dry, 
+const unsigned int DEFAULT_MAX_TEMP = 25; //a good heat
+const unsigned int DEFAULT_MIN_TEMP = 20; //getting chilly
+
+const unsigned int TEMP_MAX_LOCATION = 0;
+const unsigned int TEMP_MIN_LOCATION = 1;
+const unsigned int HUM_MAX_LOCATION = 2;
+const unsigned int HUM_MIN_LOCATION = 3;
+const char* HEATING_ON_COLOUR = "#ffb300";
+const char* HEATING_OFF_COLOUR = "#5b8080";
+const char* FAN_ON_COLOUR = "#d0c9f5";
+const char* FAN_OFF_COLOUR = "#5b8080";
+
+int unsigned currentFanSpeed = 0;
+bool heating = false;
+
+//DHT sensors
+#define S1_DHTPIN 14  // Digital pin connected to the DHT sensor
+#define S2_DHTPIN 12  // Digital pin connected to the DHT sensor
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+
 DHT dht1(S1_DHTPIN, DHTTYPE);
 DHT dht2(S2_DHTPIN, DHTTYPE);
 
+//functions
 void getMeasurements();
 void setupFan();
+void setupHeating();
 void stopFan();
 void fanSpeed(int speed);
+String fanColour();
+void heaterOn(bool shouldTurnOn);
+String heatColour();
 void registerStartup();
 void postRequest(String url, char* payload);
 void reportMeasurement(String sensorId, String measurementType, float measurement);
+void(* resetFunc) (void) = 0;
 
 // current temperature & humidity, updated in loop()
 float t1 = 0.0;
 float t2 = 0.0;
 float h1 = 0.0;
 float h2 = 0.0;
-
-int currentFanSpeed = 0;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -82,6 +100,9 @@ const long interval = 10000;
 unsigned long previousReport = 0;    // will store last time measurements were reported
 const long reportInterval = 60000; 
 
+const long msInADay = 1000 * 60 * 60 * 24;  
+unsigned long msSinceLastReset = 0;
+
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
 <head>
@@ -98,7 +119,7 @@ const char index_html[] PROGMEM = R"rawliteral(
     h2 { font-size: 3.0rem; }
     p { font-size: 3.0rem; }
     .units { font-size: 1.2rem; }
-    .dht-labels{
+    .labels{
       font-size: 1.5rem;
       vertical-align:middle;
       padding-bottom: 15px;
@@ -106,71 +127,79 @@ const char index_html[] PROGMEM = R"rawliteral(
   </style>
 </head>
 <body>
+<p>
+  <span><i id="heating" class="fas fa-fire-alt" style="color:%HEATING_COLOUR%;"></i></span>
+  <span><i id="fan" class="fas fa-wind" style="color:%FAN_COLOUR%;"></i></span>
+</p>
   <h3>Sensor 1</h3>
   <p>
+      <span>
     <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature-1">%TEMPERATURE1%</span>
+    <span id="temperature-1"> %TEMPERATURE1%</span>
     <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity-1">%HUMIDITY1%</span>
-    <sup class="units">&#37;</sup>
+    </span>
+    <span>
+      <i class="fas fa-tint" style="color:#00add6;"></i> 
+      <span id="humidity-1"> %HUMIDITY1%</span>
+      <sup class="units">&#37;</sup>
+    </span>
   </p>
     <h3>Sensor 2</h3>
   <p>
+      <span>
     <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
     <span id="temperature-2">%TEMPERATURE2%</span>
     <sup class="units">&deg;C</sup>
-  </p>
-  <p>
+    </span>
+      <span>
     <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
     <span id="humidity-2">%HUMIDITY2%</span>
     <sup class="units">&#37;</sup>
+    </span>
   </p>
 </body>
 <script>
 
-let getTemperature = (sensor) => {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = () => {
-    if (this.readyState == 4 && this.status == 200) {
-      let tempId = "temperature-"+sensor
-      console.log('applying ' + this.responseText + ' to ' + tempId);
-      document.getElementById(tempId).innerHTML = this.responseText;
-    }
+let getSensor = (sensor) => {
+  fetch('/sensor?sensor=' + sensor)
+        .then(response => response.text())
+        .then(sendorData => {
+                const parts = sendorData.split('|');
+                let id = "humidity-"+sensor
+                console.log('applying ' + parts[0] + ' to ' + id);
+                document.getElementById(id).innerHTML = parts[0];
+                
+                id = "temperature-"+sensor
+                console.log('applying ' + parts[1] + ' to ' + id);
+                document.getElementById(id).innerHTML = parts[1];
+          });
   };
-  xhttp.open("GET", "/temperature?sensor="+sensor, true);
-  xhttp.send();
-}
 
-let getHumidity = (sensor) => {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = () => {
-    if (this.readyState == 4 && this.status == 200) {
-      let humId = "humidity-"+sensor
-      console.log('applying ' + this.responseText + ' to ' + humId);
-      document.getElementById(humId).innerHTML = this.responseText;
-    }
+let getEnv = () => {
+  fetch('/env')
+        .then(response => response.text())
+        .then(envData => {
+                const parts = envData.split('|');
+                let id = "heating";
+                console.log('applying ' + parts[0] + ' to ' + id);
+                document.getElementById(id).style.color = parts[0];
+          
+                id = "fan";
+                console.log('applying ' + parts[1] + ' to ' + id);
+                document.getElementById(id).style.color = parts[1];
+         });
   };
-  xhttp.open("GET", "/humidity?sensor=" + sensor, true);
-  xhttp.send();
-}
 
-setInterval(getTemperature, 10000, '1' ) ;
-setInterval(getTemperature, 10000, '2' ) ;
-setInterval(getHumidity, 10000, '1' ) ;
-setInterval(getHumidity, 10000, '2' ) ;
+
+setInterval(getSensor, 10000, '1' ) ;
+setInterval(getSensor, 10000, '2' ) ;
+setInterval(getEnv, 10000) ;
+
 </script>
 </html>)rawliteral";
 
 // Replaces placeholder with DHT values
 String processor(const String& var){
-  //Serial.println(var);
   if(var == "TEMPERATURE1"){
     return String(t1);
   }  else if(var == "HUMIDITY1"){
@@ -179,6 +208,10 @@ String processor(const String& var){
     return String(t2);
   } else if(var == "HUMIDITY2"){
     return String(h2);
+  } else if(var == "FAN_COLOUR"){
+    return fanColour();
+  } else if(var == "HEATING_COLOUR"){
+    return heatColour();
   } else
   return String();
 }
@@ -186,10 +219,16 @@ String processor(const String& var){
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
+
+  Serial.println("\n SETTING UP \n");
+      
+  EEPROM.begin(FLASH_STOREAGE_BYTES);
+    
   dht1.begin();
   dht2.begin();
 
   setupFan();
+  setupHeating();
   
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -200,9 +239,10 @@ void setup(){
   }
 
   //secure client
-  Serial.printf("Using fingerprint '%s'\n", fingerprint);
-  client.setFingerprint(fingerprint);
-
+//  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+//  client.setFingerprint(fingerprint);
+  client.setInsecure(); //connect to HTTPS but don't validate certificate, fingerprints keep changing
+  
   // Print ESP8266 Local IP Address
   Serial.println(WiFi.localIP());
   Serial.println(apiSecret);
@@ -210,39 +250,42 @@ void setup(){
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, processor);
   });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+  
+  server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("getting data for sensor");
     char sensor[10];
+    char sensorData[50] = "";
     if(request->hasParam("sensor")) {
       strcpy(sensor, request->getParam("sensor")->value().c_str());
-          if (strcmp(sensor,"1") == 0) {
-      request->send_P(200, "text/plain", String(t1).c_str());
+      if (strcmp(sensor,"1") == 0) {
+        strcat(sensorData, String(h1).c_str());
+        strcat(sensorData, "|");
+        strcat(sensorData, String(t1).c_str());
+        Serial.println("Sending sensor response " + String(sensorData));
+        request->send_P(200, "text/plain", sensorData);
       } else if (strcmp(sensor,"2") == 0) {
-        request->send_P(200, "text/plain", String(t2).c_str());
+        strcat(sensorData, String(h2).c_str());
+        strcat(sensorData, "|");
+        strcat(sensorData, String(t2).c_str());
+        Serial.println("Sending sensor response " + String(sensorData));
+        request->send_P(200, "text/plain", sensorData);
       } else {
-      Serial.println("Unknown sensor data requested ? " +  String(sensor));
-      request->send_P(200, "text/plain", String("0").c_str());
+        Serial.println("Unknown sensor data requested ? " +  String(sensor));
+        request->send_P(200, "text/plain", String("0|0").c_str());
       }
     } else {
-      request->send_P(200, "text/plain", String("0").c_str());
+      request->send_P(200, "text/plain", String("0|0").c_str());
     }
-
   });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-        char sensor[10];
-    if(request->hasParam("sensor")) {
-      strcpy(sensor, request->getParam("sensor")->value().c_str());
-          if (strcmp(sensor,"1") == 0) {
-      request->send_P(200, "text/plain", String(h1).c_str());
-      } else if (strcmp(sensor,"2") == 0) {
-        request->send_P(200, "text/plain", String(h2).c_str());
-      } else {
-      Serial.println("Unknown sensor data requested ? " + String(sensor));
-      request->send_P(200, "text/plain", String("0").c_str());
-      }
-    } else {
-      Serial.println("Which sensor do you want data from?");
-      request->send_P(200, "text/plain", String("0").c_str());
-    }
+
+  server.on("/env", HTTP_GET, [](AsyncWebServerRequest *request){
+    char envData[50] = "";
+    Serial.println("getting data for environment control");
+    strcat(envData, heatColour().c_str());
+    strcat(envData, "|");
+    strcat(envData, fanColour().c_str());
+    Serial.println("Sending env response " + String(envData));
+    request->send_P(200, "text/plain", envData);
   });
 
   server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -253,6 +296,7 @@ void setup(){
 
   // Start server
   server.begin();
+  EEPROM.commit(); //stores any setup values
 }
  
 void loop(){  
@@ -261,6 +305,12 @@ void loop(){
     // save the last time you updated the DHT values
     previousMillis = currentMillis;
     getMeasurements();
+  }
+
+  //reset once a day because memory leaks (and I prefer to focus my time elsewehere)
+  if (msSinceLastReset > msInADay) {
+     Serial.println("resetting");
+     resetFunc();
   }
 }
 
@@ -282,6 +332,16 @@ void getMeasurements(){
     }
     else {
       t1 = newT1;
+      int maxTemp = EEPROM.read(TEMP_MAX_LOCATION);
+      int minTemp = EEPROM.read(TEMP_MIN_LOCATION);
+      
+      Serial.println("Checking temp is between " + String(minTemp) + "c and " + String(maxTemp) + "c");
+      if (t1 >= maxTemp && heating) {
+        heaterOn(false);
+      } else if (t1 <= minTemp && !heating) {
+        heaterOn(true);
+      }
+      
       Serial.println("Sensor 1 temperature " + String(t1) + "c");
       if (shouldReport) {
         reportMeasurement("1", "temperature", t1);
@@ -296,9 +356,13 @@ void getMeasurements(){
     }
     else {
       h1 = newH1;
-      if (h1 > 90) {
+      int maxHum = EEPROM.read(HUM_MAX_LOCATION);
+      int minHum = EEPROM.read(HUM_MIN_LOCATION);
+      
+      Serial.println("Checking humidity is between " + String(minHum) + "% and " + String(maxHum) + "%");
+      if (h1 >= maxHum) {
         fanSpeed(PWM_FULL);
-      } else if (h1 < 80) {
+      } else if (h1 <= minHum) {
         stopFan();
       }
       Serial.println("Sensor 1 humidity " + String(h1) + "%");
@@ -406,9 +470,46 @@ void postRequest(String url, char* content) {
 }
 
 void setupFan() {
+  //ensure fan paramewters are set
+  int maxHum = EEPROM.read(HUM_MAX_LOCATION);
+  if (maxHum == 255|| maxHum == 0) {
+    Serial.println("Storing max humidity value at " + String(HUM_MAX_LOCATION) + " with default value " + String(DEFAULT_MAX_HUMIDITY));
+    EEPROM.write(HUM_MAX_LOCATION, DEFAULT_MAX_HUMIDITY);
+  } else {
+    Serial.println("Humidity max read at " + String(HUM_MAX_LOCATION) + " is " + String(maxHum));
+  }
+
+  int minHum = EEPROM.read(HUM_MIN_LOCATION);
+  if (minHum == 255 || minHum == 0) {
+    EEPROM.write(HUM_MIN_LOCATION, DEFAULT_MIN_HUMIDITY);
+  } else {
+    Serial.println("Humidity min read at " + String(HUM_MIN_LOCATION) + " is " + String(minHum));
+  }
+  
   pinMode( Fan_PWM, OUTPUT );
   pinMode( Fan_DIR, OUTPUT );
   stopFan();
+}
+
+
+void setupHeating() {
+    //ensure heating paramewters are set
+  int maxTemp = EEPROM.read(TEMP_MAX_LOCATION);
+  if (maxTemp == 255|| maxTemp == 0) {
+    Serial.println("Storing max temp value at " + String(TEMP_MAX_LOCATION) + " with default value " + String(DEFAULT_MAX_TEMP));
+    EEPROM.write(TEMP_MAX_LOCATION, DEFAULT_MAX_TEMP);
+  } else {
+    Serial.println("Temp max read at " + String(TEMP_MAX_LOCATION) + " is " + String(maxTemp));
+  }
+
+  int minTemp = EEPROM.read(TEMP_MIN_LOCATION);
+  if (minTemp == 255 || minTemp == 0) {
+    Serial.println("Storing min temp value at " + String(TEMP_MAX_LOCATION) + " with default value " + String(DEFAULT_MAX_TEMP));
+    EEPROM.write(TEMP_MIN_LOCATION, DEFAULT_MIN_TEMP);
+  } else {
+    Serial.println("Temp min read at " + String(TEMP_MIN_LOCATION) + " is " + String(minTemp));
+  }
+  
 }
 
 void stopFan()
@@ -426,4 +527,23 @@ void fanSpeed(int speed)
   // set the motor speed and direction
   digitalWrite( Fan_DIR, HIGH );
   analogWrite( Fan_PWM, 255-speed ); // PWM speed = fast   
+}
+
+void heaterOn(bool shouldTurnOn)
+{
+  if (shouldTurnOn) {
+    Serial.println("Turning heater ON");
+    heating = shouldTurnOn;
+  } else {
+    Serial.println("Turning heater OFF");
+    heating = shouldTurnOn;
+  }
+}
+
+String heatColour() {
+  return heating ? HEATING_ON_COLOUR : HEATING_OFF_COLOUR ;
+}
+
+String fanColour() {
+  return currentFanSpeed > 0 ? FAN_ON_COLOUR : FAN_OFF_COLOUR;
 }
