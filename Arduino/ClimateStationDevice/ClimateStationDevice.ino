ca1e9a2950d09f316c8013a815f69bb1897b27a4
jamespercy <james007percy@gmail.com>
2020-06-14 02:36:23 +1000

    update setting and report heater status

--- Arduino/ClimateStationDevice/ClimateStationDevice.ino
+++ Arduino/ClimateStationDevice/ClimateStationDevice.ino
@@ -1,6 +1,5 @@
 /*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com/esp8266-dht11dht22-temperature-and-humidity-web-server-with-arduino-ide/
+  based on project details at https://randomnerdtutorials.com/esp8266-dht11dht22-temperature-and-humidity-web-server-with-arduino-ide/
 *********/
 
 // Import required libraries
@@ -65,6 +64,7 @@ DHT dht1(S1_DHTPIN, DHTTYPE);
 DHT dht2(S2_DHTPIN, DHTTYPE);
 
 //functions
+void setupWebServer();
 void getMeasurements();
 void setupFan();
 void setupHeating();
@@ -77,6 +77,14 @@ void registerStartup();
 void postRequest(String url, char* payload);
 void reportMeasurement(String sensorId, String measurementType, float measurement);
 void(* resetFunc) (void) = 0;
+int getMinTemp();
+int getMaxTemp();
+int getMinHum();
+int getMaxHum();
+void setMinTemp(int minTemp);
+void setMaxTemp(int maxTemp);
+void setMinHum(int minHum);
+void setMaxHum(int maxHum);
 
 // current temperature & humidity, updated in loop()
 float t1 = 0.0;
@@ -157,8 +165,51 @@ const char index_html[] PROGMEM = R"rawliteral(
     <sup class="units">&#37;</sup>
     </span>
   </p>
+  <p>
+  <i id="settings" class="fas fa-cogs" style="color:#5b8080;"></i>
+    <form id="settings-form" style="display:none">
+    <div style="border: 1px dashed green;">
+      <label for="minTemp">Min temperature:</label><br>
+      <input type="number" id="minTemp" name="minTemp" value="%MIN_TEMP%" oninput="this.value = Math.abs(Math.round(this.value));" min="15" max="30"  size="10"><br>
+      <label for="maxTemp">Max temperature:</label><br>
+      <input type="number" id="maxTemp" name="maxTemp" value="%MAX_TEMP%" oninput="this.value = Math.abs(Math.round(this.value));" min="15" max="30"  size="10"><br>
+      <label for="minHum">Min humidity:</label><br>
+      <input type="number" id="minHum" name="minHum" value="%MIN_HUM%" oninput="this.value = Math.abs(Math.round(this.value));" min="50" max="99"  size="10"><br>
+      <label for="maxHum">Max humidity:</label><br>
+      <input type="number" id="maxHum" name="maxHum" value="%MAX_HUM%" oninput="this.value = Math.abs(Math.round(this.value));" min="50" max="99"  size="10"><br>
+      <br>
+      <button id="update">Update</button><button id="cancel">Cancel</button>
+      </div>
+    </form>
+  </p>
 </body>
 <script>
+  
+document.getElementById('settings').addEventListener('click', function (event) {
+  event.preventDefault();
+  document.getElementById('settings').style.display = 'none';
+  document.getElementById('settings-form').style.display = '';
+}, false);
+
+document.getElementById('update').addEventListener('click', function (event) {
+  event.preventDefault();
+  let minTemp = document.getElementById('minTemp').value;
+  let maxTemp = document.getElementById('maxTemp').value;
+  let minHum = document.getElementById('minHum').value;
+  let maxHum = document.getElementById('maxHum').value;
+  fetch(`/update-settings?minTemp=${minTemp}&maxTemp=${maxTemp}&minHum=${minHum}&maxHum=${maxHum}`);
+
+  document.getElementById('settings').style.display = '';
+  document.getElementById('settings-form').style.display = 'none';
+
+}, false);
+
+document.getElementById('cancel').addEventListener('click', function (event) {
+  event.preventDefault()
+  document.getElementById('settings').style.display = '';
+  document.getElementById('settings-form').style.display = 'none';
+
+}, false);
 
 let getSensor = (sensor) => {
   fetch('/sensor?sensor=' + sensor)
@@ -212,7 +263,16 @@ String processor(const String& var){
     return fanColour();
   } else if(var == "HEATING_COLOUR"){
     return heatColour();
-  } else
+  } else if(var == "MIN_TEMP"){
+    return String(getMinTemp());
+  } else if(var == "MAX_TEMP"){
+    return String(getMaxTemp());
+  } else if(var == "MIN_HUM"){
+    return String(getMinHum());
+  } else if(var == "MAX_HUM"){
+    return String(getMaxHum());
+  }
+  else
   return String();
 }
 
@@ -245,52 +305,9 @@ void setup(){
   
   // Print ESP8266 Local IP Address
   Serial.println(WiFi.localIP());
-  Serial.println(apiSecret);
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
   
-  server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial.println("getting data for sensor");
-    char sensor[10];
-    char sensorData[50] = "";
-    if(request->hasParam("sensor")) {
-      strcpy(sensor, request->getParam("sensor")->value().c_str());
-      if (strcmp(sensor,"1") == 0) {
-        strcat(sensorData, String(h1).c_str());
-        strcat(sensorData, "|");
-        strcat(sensorData, String(t1).c_str());
-        Serial.println("Sending sensor response " + String(sensorData));
-        request->send_P(200, "text/plain", sensorData);
-      } else if (strcmp(sensor,"2") == 0) {
-        strcat(sensorData, String(h2).c_str());
-        strcat(sensorData, "|");
-        strcat(sensorData, String(t2).c_str());
-        Serial.println("Sending sensor response " + String(sensorData));
-        request->send_P(200, "text/plain", sensorData);
-      } else {
-        Serial.println("Unknown sensor data requested ? " +  String(sensor));
-        request->send_P(200, "text/plain", String("0|0").c_str());
-      }
-    } else {
-      request->send_P(200, "text/plain", String("0|0").c_str());
-    }
-  });
-
-  server.on("/env", HTTP_GET, [](AsyncWebServerRequest *request){
-    char envData[50] = "";
-    Serial.println("getting data for environment control");
-    strcat(envData, heatColour().c_str());
-    strcat(envData, "|");
-    strcat(envData, fanColour().c_str());
-    Serial.println("Sending env response " + String(envData));
-    request->send_P(200, "text/plain", envData);
-  });
-
-  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/favicon.ico", "image/png");
-  });
+  // Route for root / web page
+  setupWebServer();
 
   registerStartup();
 
@@ -332,8 +349,8 @@ void getMeasurements(){
     }
     else {
       t1 = newT1;
-      int maxTemp = EEPROM.read(TEMP_MAX_LOCATION);
-      int minTemp = EEPROM.read(TEMP_MIN_LOCATION);
+      int maxTemp = getMaxTemp();
+      int minTemp = getMinTemp();
       
       Serial.println("Checking temp is between " + String(minTemp) + "c and " + String(maxTemp) + "c");
       if (t1 >= maxTemp && heating) {
@@ -356,8 +373,8 @@ void getMeasurements(){
     }
     else {
       h1 = newH1;
-      int maxHum = EEPROM.read(HUM_MAX_LOCATION);
-      int minHum = EEPROM.read(HUM_MIN_LOCATION);
+      int maxHum = getMaxHum();
+      int minHum = getMinHum();
       
       Serial.println("Checking humidity is between " + String(minHum) + "% and " + String(maxHum) + "%");
       if (h1 >= maxHum) {
@@ -398,6 +415,7 @@ void getMeasurements(){
 
     if (shouldReport) {
       reportMeasurement("fan", "speed", currentFanSpeed);
+      reportMeasurement("heater", "on", heating ? 1 : 0);
     }
 }
 
@@ -471,17 +489,17 @@ void postRequest(String url, char* content) {
 
 void setupFan() {
   //ensure fan paramewters are set
-  int maxHum = EEPROM.read(HUM_MAX_LOCATION);
+  int maxHum = getMaxHum();
   if (maxHum == 255|| maxHum == 0) {
     Serial.println("Storing max humidity value at " + String(HUM_MAX_LOCATION) + " with default value " + String(DEFAULT_MAX_HUMIDITY));
-    EEPROM.write(HUM_MAX_LOCATION, DEFAULT_MAX_HUMIDITY);
+    setMaxHum(DEFAULT_MAX_HUMIDITY);
   } else {
     Serial.println("Humidity max read at " + String(HUM_MAX_LOCATION) + " is " + String(maxHum));
   }
 
-  int minHum = EEPROM.read(HUM_MIN_LOCATION);
+  int minHum = getMinHum();
   if (minHum == 255 || minHum == 0) {
-    EEPROM.write(HUM_MIN_LOCATION, DEFAULT_MIN_HUMIDITY);
+    setMinHum(DEFAULT_MIN_HUMIDITY);
   } else {
     Serial.println("Humidity min read at " + String(HUM_MIN_LOCATION) + " is " + String(minHum));
   }
@@ -494,18 +512,18 @@ void setupFan() {
 
 void setupHeating() {
     //ensure heating paramewters are set
-  int maxTemp = EEPROM.read(TEMP_MAX_LOCATION);
+  int maxTemp = getMaxTemp();
   if (maxTemp == 255|| maxTemp == 0) {
     Serial.println("Storing max temp value at " + String(TEMP_MAX_LOCATION) + " with default value " + String(DEFAULT_MAX_TEMP));
-    EEPROM.write(TEMP_MAX_LOCATION, DEFAULT_MAX_TEMP);
+    setMaxTemp(DEFAULT_MAX_TEMP);
   } else {
     Serial.println("Temp max read at " + String(TEMP_MAX_LOCATION) + " is " + String(maxTemp));
   }
 
-  int minTemp = EEPROM.read(TEMP_MIN_LOCATION);
+  int minTemp = getMinTemp();
   if (minTemp == 255 || minTemp == 0) {
     Serial.println("Storing min temp value at " + String(TEMP_MAX_LOCATION) + " with default value " + String(DEFAULT_MAX_TEMP));
-    EEPROM.write(TEMP_MIN_LOCATION, DEFAULT_MIN_TEMP);
+    setMinTemp(DEFAULT_MIN_TEMP);
   } else {
     Serial.println("Temp min read at " + String(TEMP_MIN_LOCATION) + " is " + String(minTemp));
   }
@@ -546,4 +564,129 @@ String heatColour() {
 
 String fanColour() {
   return currentFanSpeed > 0 ? FAN_ON_COLOUR : FAN_OFF_COLOUR;
+}
+
+int getMinTemp() {
+  return EEPROM.read(TEMP_MIN_LOCATION);
+}
+
+int getMaxTemp() {
+  return EEPROM.read(TEMP_MAX_LOCATION);
+}
+
+int getMinHum() {
+  return EEPROM.read(HUM_MIN_LOCATION);
+}
+
+int getMaxHum() {
+  return EEPROM.read(HUM_MAX_LOCATION);
+}
+
+void setMinTemp(int minTemp) {
+  if (minTemp < 15 || minTemp > 29 || minTemp >= getMaxTemp()) {
+    Serial.println("Received invalid min temp " + String(minTemp));
+    return;
+  }
+  Serial.println("Setting min temp as " + String(minTemp));
+  EEPROM.write(TEMP_MIN_LOCATION, minTemp);
+  EEPROM.commit();
+}
+
+void setMaxTemp(int maxTemp) {
+  if (maxTemp < 16 || maxTemp > 30 || maxTemp <= getMinTemp()) {
+    Serial.println("Received invalid max temp " + String(maxTemp));
+    return;
+  }
+  Serial.println("Setting max temp as " + String(maxTemp));
+  EEPROM.write(TEMP_MAX_LOCATION, maxTemp);
+  EEPROM.commit();
+}
+
+void setMinHum(int minHum) {
+    if (minHum < 50 || minHum > 98 || minHum >= getMaxHum()) {
+    Serial.println("Received invalid min hummidity " + String(minHum));
+    return;
+  }
+  Serial.println("Setting min humidity as " + String(minHum));
+  EEPROM.write(HUM_MIN_LOCATION, minHum);
+  EEPROM.commit();
+}
+
+void setMaxHum(int maxHum) {
+    if (maxHum < 51 || maxHum > 99 || maxHum <= getMinHum()) {
+    Serial.println("Received invalid max hummidity " + String(maxHum));
+    return;
+  }
+    Serial.println("Setting max humidity as " + String(maxHum));
+  EEPROM.write(HUM_MAX_LOCATION, maxHum);
+  EEPROM.commit();
+}
+
+void setupWebServer() {
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  server.on("/update-settings", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Serial.println("updating settings");
+    
+   if(request->hasParam("minTemp")) {
+      setMinTemp(atoi(request->getParam("minTemp")->value().c_str()));
+    } else {
+      Serial.println("no minTemp parameter found");
+    }
+   if(request->hasParam("maxTemp")) {
+      setMaxTemp(atoi(request->getParam("maxTemp")->value().c_str()));
+    } else {
+      Serial.println("no maxTemp parameter found");
+    }
+   if(request->hasParam("minHum")) {
+      setMinHum(atoi(request->getParam("minHum")->value().c_str()));
+    } else {
+      Serial.println("no minHum parameter found");
+    }
+   if(request->hasParam("maxHum")) {
+      setMaxHum(atoi(request->getParam("maxHum")->value().c_str()));
+    } else {
+      Serial.println("no maxHum parameter found");
+    }
+    request->send_P(200, "text/plain", String("OK").c_str());
+  });
+  
+  server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request){
+//    Serial.println("getting data for sensor");
+    char sensor[10];
+    char sensorData[50] = "";
+    if(request->hasParam("sensor")) {
+      strcpy(sensor, request->getParam("sensor")->value().c_str());
+      if (strcmp(sensor,"1") == 0) {
+        strcat(sensorData, String(h1).c_str());
+        strcat(sensorData, "|");
+        strcat(sensorData, String(t1).c_str());
+        request->send_P(200, "text/plain", sensorData);
+      } else if (strcmp(sensor,"2") == 0) {
+        strcat(sensorData, String(h2).c_str());
+        strcat(sensorData, "|");
+        strcat(sensorData, String(t2).c_str());
+        request->send_P(200, "text/plain", sensorData);
+      } else {
+        Serial.println("Unknown sensor data requested ? " +  String(sensor));
+        request->send_P(200, "text/plain", String("0|0").c_str());
+      }
+    } else {
+      request->send_P(200, "text/plain", String("0|0").c_str());
+    }
+  });
+
+  server.on("/env", HTTP_GET, [](AsyncWebServerRequest *request){
+    char envData[50] = "";
+    strcat(envData, heatColour().c_str());
+    strcat(envData, "|");
+    strcat(envData, fanColour().c_str());
+    request->send_P(200, "text/plain", envData);
+  });
+
+  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/favicon.ico", "image/png");
+  });
 }
